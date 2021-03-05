ba9c4e84b0e6c5dcbadcc6be29ec149e94b1bbbf
B-Griffinn <bigriffin94@outlook.com>
2021-03-05 13:33:23 -0800

    base styles added to home page

--- src/components/pages/HomePage/RenderHomePage.tsx
+++ src/components/pages/HomePage/RenderHomePage.tsx
@@ -4,7 +4,6 @@ import HomePageInfo from './HomePageInfo';
 const RenderHomePage = (): React.ReactElement => {
   return (
     <div className="home-page-wrapper">
-      <h1>home</h1>
       <HomePageInfo />
     </div>
   );
